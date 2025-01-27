1a63b3c3f38eec359fcc0918ca5044fcccb5b5a4
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2025-01-27 21:54:10 +0330

    A-45

---
+++ 6.5/Tehran House Price/lls.py
@@ -0,0 +1,23 @@
+import numpy as np
+from numpy.linalg import inv
+
+class LinearLeastSquare:
+    def __init__(self):
+        self.w = None
+
+    def fit(self,X_train,Y_train):
+        self.w=inv(X_train.T @ X_train)@ X_train.T @ Y_train
+        return self.w
+
+    def predict(self,X_test):
+        Y_pred= X_test @ self.w
+        return Y_pred
+    
+    def evaluate(self,Y_test,Y_pred,metric):
+        if metric== "mae":
+            loss = np.sum(np.abs(Y_test-Y_pred))/len(Y_test)
+        elif metric == "mse":
+            loss = np.sum((Y_test-Y_pred)**2)/len(Y_test)
+        elif metric == "rmse":
+            loss = np.sqrt(np.sum((Y_test-Y_pred)**2)/len(Y_test))
+        return loss
