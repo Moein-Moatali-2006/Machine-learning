e52ca99f356fb864e48fbb4e2a88a7e6ad557955
Moein-Moatali-2006 <MoeinMoatali@gmail.com>
2025-02-06 17:23:34 +0330

    A-46

--- Perceptron/Boston house-prices/perceptron.py
+++ Perceptron/Boston house-prices/perceptron.py
@@ -1,33 +1,63 @@
 import numpy as np
 
-
 class Perceptron:
-    def __init__(self,learning_rate,epochs):
-        self.w = np.random.rand(2,1)
-        self.b = np.random.rand()
-        self.learning_rate_w = learning_rate
-        self.learning_rate_b = learning_rate
-        self.epochs = epochs
-        self.loss_history = [] # MSE
+    def __init__(self,learning_rate,input_length):
+        self.learning_rate = learning_rate
+        self.weights = np.random.rand(input_length)
+        self.bias = np.random.rand(1)
 
-    def fit(self,X_train,Y_train):
-        for j in range(self.epochs):
-            loss = 0
-            for i in range(X_train.shape[0]):
-                x = X_train[i].reshape(1, -1)
-                y = Y_train[i]
-                y_pred = x @ self.w + self.b
-                error = y - y_pred
-                # update w and b
-                self.w = self.w + self.learning_rate_w * (x.T * error)
-                self.b = self.b + self.learning_rate_b * np.sum(error)
-                loss += float(error ** 2)
-            self.loss_history.append(loss)
-    
+    def activation(self,x,function):
+        if function == "sigmoid":
+            return 1 / (1 + np.exp(-x))
+        elif function == "relu":
+            return np.maximum(0,x)
+        elif function == "tanh":
+            return np.tanh(x)
+        elif function == "linear":
+            return x
+        else:
+            raise Exception("Activation function")
+
+    def fit(self,X_train,Y_train,epochs):
+        for epoch in range(epochs):
+            for x,y in zip(X_train,Y_train):
+                # forwarding 
+                y_pred = x @ self.weights + self.bias 
+                y_pred = self.activation(y_pred,"sigmoid")
+                # back propagation
+                error = y -y_pred
+                # updating
+                self.weights += self.learning_rate * error * x
+                self.bias += self.learning_rate * error
+
     def predict(self,X_test):
-        return X_test @ self.w + self.b
+        Y_pred = []
+        for x_test in X_test:
+            y_pred = x_test @ self.weights + self.bias
+            y_pred = self.activation(y_pred,"sigmoid")
+            Y_pred.append(y_pred)
+        return np.array(Y_pred)
+    
+    def calculate_loss(self,X_test,Y_test,metric):
+        Y_pred = self.predict(X_test)
+        if metric == "mse":
+            return np.mean(np.square(Y_test - Y_pred))
+        elif metric == "mae":
+            return np.mean(np.abs(Y_test - Y_pred))
+        elif metric == "rmse":
+            return np.sqrt(np.mean(np.square(Y_test - Y_pred)))
+        else:
+            raise Exception("Error")
+
+    def calculate_accuracy(self,X_test,Y_test):
+        Y_pred = self.predict(X_test)
+        Y_pred = Y_pred.reshape(-1)
+        Y_pred = np.where(Y_pred > 0.5 , 1 , 0 )
+        accuracy = np.sum(Y_pred == Y_test) / len(Y_test)
+        return accuracy
 
     def evaluate(self,X_test,Y_test):
-        y_pred = self.predict(X_test)
-        mse = np.mean((y_pred - Y_test)**2)
-        return mse
+        loss = self.calculate_loss(X_test,Y_test,metric="mse")
+        accuracy = self.calculate_accuracy(X_test,Y_test)
+
+        return loss,accuracy
